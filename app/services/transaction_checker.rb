824120be6fbe118293146f167621fd4cc220e89d
Danil Pismenny <danil@brandymint.ru>
2021-11-19 14:37:46 +0300

    minor fix

--- app/services/transaction_checker.rb
+++ app/services/transaction_checker.rb
@@ -4,7 +4,7 @@ class TransactionChecker
   def check!(txid, cc_code)
     addresses = AddressFinder
                 .new
-                .income_addresses_of_transaction(txid)
+                .income_addresses_of_transaction(txid, cc_code)
                 .reject { |address| AddressAnalysis.actual?(address) }
 
     Rails.logger.info("Found addresses #{addresses.join(',')} for #{cc_code}")
