42c267c632b2fdba3b2ec3bb8a01e2130434e686
Danil Pismenny <danil@brandymint.ru>
2021-11-19 14:55:02 +0300

    Improve UI

--- app/daemons/daemons/income_transactions_analyser.rb
+++ app/daemons/daemons/income_transactions_analyser.rb
@@ -10,11 +10,15 @@ module Daemons
     # CC_CODES = ["REEF", "BCD", "SONO", "ETC", "BTG", "DAI", "DOGE", "MDT", "BEAM", "DASH", "PIVX", "BTC", "USDC", "USDT", "LTC", "MCR", "USDM", "RUBM", "BCH", "ETH"]
     CC_CODES = %w[BTC]
 
+    def self.scope
+      BlockchainTx
+        .where(cc_code: CC_CODES)
+    end
+
     def process
       Rails.logger.info('Start process')
       TransactionSource.find_each do |transaction_source|
-        BlockchainTx
-          .where(cc_code: CC_CODES)
+          self.class.scope
           .where('id > ?', transaction_source.last_processed_blockchain_tx_id)
           .find_in_batches(batch_size: BATCH_SIZE) do |batch|
           batch.each do |btx|
