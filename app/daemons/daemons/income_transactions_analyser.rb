fbe76d11648ac6215edde5cdcc8bd0fc50dfc61d
Danil Pismenny <danil@brandymint.ru>
2021-11-19 14:22:59 +0300

    rubocop

--- app/daemons/daemons/income_transactions_analyser.rb
+++ app/daemons/daemons/income_transactions_analyser.rb
@@ -9,15 +9,15 @@ module Daemons
 
     def process
       Rails.logger.info('Start process')
-      TransactionSource.find_each do |_ts|
+      TransactionSource.find_each do |transaction_source|
         BlockchainTx
-          .where('id > ?', tx.last_processed_blockchain_tx_id)
+          .where('id > ?', transaction_source.last_processed_blockchain_tx_id)
           .order(:id)
           .find_in_batches(batch_size: BATCH_SIZE) do |batch|
           batch.each do |btx|
             Rails.logger.info("Process #{btx.txid}")
             TransactionChecker.new.check! btx.txid
-            tx.update! last_processed_blockchain_tx_id: btx.id
+            transaction_source.update! last_processed_blockchain_tx_id: btx.id
           end
         end
       end
