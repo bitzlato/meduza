a066bfa8fbe8f058d3991cfdf631a3524a4b55d8
Danil Pismenny <danil@brandymint.ru>
2021-11-24 18:24:17 +0300

    Fix model TransactionAnalysis

--- app/models/transaction_analysis.rb
+++ app/models/transaction_analysis.rb
@@ -1,7 +1,9 @@
 class TransactionAnalysis < ApplicationRecord
-  belongs_to :analysis_result
+  belongs_to :analysis_result, optional: true
 
   upsert_keys [:txid]
+
+  validates :input_addresses, presence: true, unless: :analysis_result
 
   def self.actual?(txid)
     ta = find_by(txid: txid)
