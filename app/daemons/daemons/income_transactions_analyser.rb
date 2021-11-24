10a8ab0a783ef3ef5b6c1a7990530cb56fd65f85
Danil Pismenny <danil@brandymint.ru>
2021-11-24 11:29:09 +0300

    Add ANALYZABLES_CODES

--- app/daemons/daemons/income_transactions_analyser.rb
+++ app/daemons/daemons/income_transactions_analyser.rb
@@ -7,18 +7,36 @@ module Daemons
     SLEEP_INTERVAL = 10 # seconds
     BATCH_SIZE = 100
 
-    ETH_CC_CODES = %w[ETH MDT ETC DAI USDC USDT MCR].freeze
+    # Result of ValegaClient.new.risk_assets_types
+    #
+    VALEGA_ASSETS_TYPES = [{"id"=>"wvaVWgVy9p", "name"=>"Bitcoin", "code"=>"BTC"},
+                           {"id"=>"Qkqz8909GX", "name"=>"Ethereum", "code"=>"ETH"},
+                           {"id"=>"9xLV1MVON2", "name"=>"XRP", "code"=>"XRP"},
+                           {"id"=>"YqGzbE79Nw", "name"=>"Tether", "code"=>"USDT"},
+                           {"id"=>"dMj7xB023b", "name"=>"Bitcoin Cash", "code"=>"BCH"},
+                           {"id"=>"9EPVJjVGo1", "name"=>"Bitcoin SV", "code"=>"BSV"},
+                           {"id"=>"Xd90dEVyOe", "name"=>"Litecoin", "code"=>"LTC"},
+                           {"id"=>"6k8zB5729g", "name"=>"Dash", "code"=>"DASH"},
+                           {"id"=>"Bq60REzRnk", "name"=>"Zcash", "code"=>"ZEC"},
+                           {"id"=>"3qe7g4zaQk", "name"=>"Stellar", "code"=>"XLM"},
+                           {"id"=>"dpbzOGzkJ5", "name"=>"USD Coin", "code"=>"USDC"}]
+
+    VALEGA_ASSETS_CODES = Set.new(VALEGA_ASSETS_CODES.map { |c| c.fetch('code') }).freeze
+
+    ETHEREUM_CODES = %w[MDT ETC DAI USDC USDT MCR].freeze
     BITCOIN_FORKS = %w[BTC DOGE LTC DASH BCH].freeze
-    CC_CODES = BITCOIN_FORKS + ETH_CC_CODES
+    OUR_CODES = Set.new(ETHEREUM_CODES + BITCOIN_FORKS).freeze
+
+    ANALYZABLE_CODES = VALEGA_ASSETS_CODES.intersection OUR_CODES
 
     def self.scope
       BlockchainTx
-        .where("source ->> 'category' = 'receive'")
-        .where(cc_code: CC_CODES)
+        .income
+        .where(cc_code: ANALYZABLE_CODES.to_a)
     end
 
     def process
-      Rails.logger.info('Start process')
+      Rails.logger.info("Start process with #{ANALYZABLE_CODES.to_a.join(',')} analyzable codes")
       TransactionSource.find_each do |transaction_source|
         self.class.scope
             .where('id > ?', transaction_source.last_processed_blockchain_tx_id)
--- app/models/blockchain_tx.rb
+++ app/models/blockchain_tx.rb
@@ -1,3 +1,5 @@
 class BlockchainTx < ApplicationRecord
   self.table_name = :blockchain_tx
+
+  scope :income, -> { where("source ->> 'category' = 'receive'") }
 end
