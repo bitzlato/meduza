98377d098ae18e2b0cd9a9e9bc963cdac19530cb
Danil Pismenny <danil@brandymint.ru>
2021-11-26 11:57:53 +0300

    Add search by address in TransactionAnalysis

--- app/models/transaction_analysis.rb
+++ app/models/transaction_analysis.rb
@@ -3,6 +3,8 @@ class TransactionAnalysis < ApplicationRecord
 
   upsert_keys [:txid]
 
+  scope :include_address, -> (address) { where("input_addresses::jsonb ? :address", address: address) }
+
   # TODO seach by input_address
 
   validates :input_addresses, presence: true, unless: :analysis_result
