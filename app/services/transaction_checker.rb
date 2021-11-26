bc0ab7b8e88665fba8a71c847b347ed0cd521a15
Danil Pismenny <danil@brandymint.ru>
2021-11-26 11:58:06 +0300

    Refactor TransactionChecker

--- app/services/transaction_checker.rb
+++ app/services/transaction_checker.rb
@@ -11,20 +11,24 @@ class TransactionChecker
       .income_addresses_of_transaction(txid, cc_code)
       .presence
 
-    if addresses.blank?
-      Rails.logger.warn("No addresses found for transaction #{txid} (#{cc_code})")
-      return ValegaAnalyzer.new.analyze_transaction(txid, cc_code)
+    if addresses.any?
+      Rails.logger.info("Found addresses #{addresses.join(',')} for #{cc_code}")
+      addresses_to_analyze = addresses.reject { |address| AddressAnalysis.actual?(address) }.compact
+      ValegaAnalyzer.new.analyze_addresses(addresses_to_analyze, cc_code) if addresses_to_analyze.any?
     end
 
-    Rails.logger.info("Found addresses #{addresses.join(',')} for #{cc_code}")
+    ValegaAnalyzer.new.analyze_transaction(txid, cc_code)
 
-    addresses_to_analyze = addresses.reject { |address| AddressAnalysis.actual?(address) }.compact
+    # Узнаем на основе данных с valega
+    # upsert_transaction_with_addresses addresses
+  end
 
-    return ValegaAnalyzer.new.analyze_transaction(txid, cc_code) if addresses_to_analyze.blank?
+  private
 
-    ValegaAnalyzer.new.analyze_addresses(addresses_to_analyze, cc_code) if addresses_to_analyze.any?
+  def upsert_transaction_with_addresses(addresses)
     risk_level = AddressAnalysis.where(address: addresses).maximum(:risk_level)
-    risk_confidence = AddressAnalysis.where(address: addresses).minimum(:risk_level)
+    risk_confidence = AddressAnalysis.where(address: addresses).minimum(:risk_confidence)
+
     TransactionAnalysis.upsert!(
       txid: txid,
       cc_code: cc_code,
