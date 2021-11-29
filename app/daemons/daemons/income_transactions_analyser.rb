450148f8fdbdff904425cca6bd72bffe5427ccd3
Danil Pismenny <danil@brandymint.ru>
2021-11-29 22:06:06 +0300

    Improve daemon quit

--- app/daemons/daemons/income_transactions_analyser.rb
+++ app/daemons/daemons/income_transactions_analyser.rb
@@ -4,8 +4,8 @@ module Daemons
   # Проверяет эти адреса через valega и отмечает в базе
   #
   class IncomeTransactionsAnalyser < Base
-    SLEEP_INTERVAL = 1 # seconds
-    BATCH_SIZE = ValegaClient::MAX_ELEMENTS
+    @sleep_time = 2.seconds
+
 
     VALEGA_ASSETS_CODES = Set.new(ValegaClient::ASSETS_TYPES.map { |c| c.fetch('code') }).freeze
 
@@ -25,23 +25,23 @@ module Daemons
       Rails.logger.info("Start process with #{ANALYZABLE_CODES.to_a.join(',')} analyzable codes")
       ANALYZABLE_CODES.each do |cc_code|
         TransactionSource.where(cc_code: cc_code).find_each do |transaction_source|
-          self.class.scope
-              .where('id > ?', transaction_source.last_processed_blockchain_tx_id)
-              .find_in_batches(batch_size: BATCH_SIZE) do |btxs|
-            Rails.logger.info("Process id=#{btxs.pluck(:id).join(',')} for #{cc_code}")
-            ValegaAnalyzer.new.analyze_transaction btxs, cc_code
-            transaction_source.update! last_processed_blockchain_tx_id: btxs.pluck(:id).max
-          rescue ValegaClient::TooManyRequests => err
-            report_exception err, true
-            Rails.logger.error "Retry: #{err.message}"
-            sleep 1
-            retry
-          end
+          btxs = self.class.scope
+            .where('id > ?', transaction_source.last_processed_blockchain_tx_id)
+            .order(:id)
+            .limit(ValegaClient::MAX_ELEMENTS)
+
+          next unless btxs.any?
+          Rails.logger.info("Process id=#{btxs.pluck(:id).join(',')} for #{cc_code}")
+          ValegaAnalyzer.new.analyze_transaction btxs, cc_code
+          transaction_source.update! last_processed_blockchain_tx_id: btxs.maximum(:id)
+        rescue ValegaClient::TooManyRequests => err
+          report_exception err, true
+          Rails.logger.error "Retry: #{err.message}"
+          sleep 1
+          retry
         end
-        Rails.logger.info("Sleep for #{SLEEP_INTERVAL}")
-        sleep SLEEP_INTERVAL
       rescue StandardError => e
-        report_exception e
+        report_exception e, true, cc_code: cc_code
       end
     end
   end
