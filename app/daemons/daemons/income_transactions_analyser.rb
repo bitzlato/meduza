eb2b3e61712718277a3386b6c83a7d21aa1e1df8
Danil Pismenny <danil@brandymint.ru>
2021-11-29 21:57:10 +0300

    Add ETH to supported cc codes

--- app/daemons/daemons/income_transactions_analyser.rb
+++ app/daemons/daemons/income_transactions_analyser.rb
@@ -9,7 +9,7 @@ module Daemons
 
     VALEGA_ASSETS_CODES = Set.new(ValegaClient::ASSETS_TYPES.map { |c| c.fetch('code') }).freeze
 
-    ETHEREUM_CODES = %w[MDT ETC DAI USDC USDT MCR].freeze
+    ETHEREUM_CODES = %w[ETH MDT ETC DAI USDC USDT MCR].freeze
     BITCOIN_FORKS = %w[BTC DOGE LTC DASH BCH].freeze
     OUR_CODES = Set.new(ETHEREUM_CODES + BITCOIN_FORKS).freeze
 
