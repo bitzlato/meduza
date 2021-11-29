c6a1e16d540d429416bd56a7c03abb10fd7476ae
Danil Pismenny <danil@brandymint.ru>
2021-11-29 22:42:02 +0300

    Add analzyed users controller

---
+++ app/controllers/analyzed_users_controller.rb
@@ -0,0 +1,15 @@
+class AnalyzedUsersController < ApplicationController
+  include PaginationSupport
+
+  layout 'fluid'
+
+  def index
+    scope = AnalyzedUsers
+      order('risk_level_3_count desc')
+    render locals: { analyzed_users: paginate(scope) }
+  end
+
+  def show
+    render locals: { analyzed_user: AnalyzedUser.find(params[:id]) }
+  end
+end
