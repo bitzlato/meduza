74119a70c07edeef9c2ab8a26ca18d97e8fc7fa0
Danil Pismenny <danil@brandymint.ru>
2021-11-30 09:31:02 +0300

    Improve analyzed users

--- app/models/analyzed_user.rb
+++ app/models/analyzed_user.rb
@@ -3,4 +3,8 @@ class AnalyzedUser < ApplicationRecord
   upsert_keys [:user_id]
 
   has_many :transaction_analyses
+
+  def last_risked_transaction_analysis
+    @last_risked_transaction_analysis ||= transaction_analyses.where(risk_level: 3, risk_confidence: 1.0).last
+  end
 end
