a9ecc2c5d29143a98e2c2f36f40fac73490f8ef8
Danil Pismenny <danil@brandymint.ru>
2022-02-21 14:31:20 +0300

    m

--- app/controllers/concerns/hided_columns.rb
+++ app/controllers/concerns/hided_columns.rb
@@ -7,7 +7,7 @@ module HidedColumns
     before_action do
       session[hided_columns_key] = params[:hide_columns] if params.key? :hide_columns
     end
-    helper_method :hided_columns, :hided_columns_key
+    helper_method :hided_columns
   end
 
   private
--- app/helpers/hidden_columns_helper.rb
+++ app/helpers/hidden_columns_helper.rb
@@ -1,13 +1,15 @@
 module HiddenColumnsHelper
   def hide_column(column)
-    link_to '[×]', url_for(q: params.fetch(:q, {}).permit!, hide_columns: hided_columns + [column]), class: 'ml-2'
-  end
-
-  def unhide_all_url
-    url_for(hide_columns: [])
+    link_to '[×]', url_for(
+      q: params.fetch(:q, {}).permit!,
+      hide_columns: hided_columns + [column]
+    ), class: 'ml-2'
   end
 
   def unhide_column_url(column)
-    url_for(q: params.fetch(:q, {}).permit!, hide_columns: hided_columns - [column])
+    url_for(
+      q: params.fetch(:q, {}).permit!,
+      hide_columns: hided_columns - [column] + ['_']
+    )
   end
 end
