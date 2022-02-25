c10f5449f8b0d84d5c7d13179f0c61996bf72ab5
Danil Pismenny <danil@brandymint.ru>
2022-02-25 14:28:01 +0300

    m

--- app/daemons/daemons/legacy_pender.rb
+++ app/daemons/daemons/legacy_pender.rb
@@ -17,7 +17,7 @@ module Daemons
           .order(:id)
           .find_each do |btx|
           Rails.logger.info("Put to penging_transaction #{btx.id}: #{btx.txid} #{cc_code}")
-          TransactionAnalysis.create!(txid: btx.txid, cc_code: btx.cc_code) unless btz.transaction_analyses.present?
+          TransactionAnalysis.create!(txid: btx.txid, cc_code: btx.cc_code) unless btx.transaction_analyses.present?
           transaction_source.update! last_processed_blockchain_tx_id: btx.id if btz.id > transaction_source.last_processed_blockchain_tx_id
         end
         break unless @running
