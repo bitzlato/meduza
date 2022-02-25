5a2401fe4263b766d668ba03cdbfa28e9605e11b
Danil Pismenny <danil@brandymint.ru>
2022-02-25 18:02:25 +0300

    m

--- app/daemons/daemons/legacy_pender.rb
+++ app/daemons/daemons/legacy_pender.rb
@@ -8,7 +8,7 @@ module Daemons
     # TODO Проверять в одной валеговской транзкции сразу все транзакции по разным валютам
     def process
       Rails.logger.info("[LegacyPender] Start process with #{ANALYZABLE_CODES.to_a.join(',')} analyzable codes")
-      ANALYZABLE_CODES.each do |cc_code|
+      count = ANALYZABLE_CODES.each do |cc_code|
         transaction_source = TransactionSource.find_or_create_by!(cc_code: cc_code)
         transaction_source.reload
         self.class.scope
@@ -20,7 +20,8 @@ module Daemons
           Rails.logger.info("Put to penging_transaction #{btx.id}: #{btx.txid} #{cc_code}")
           TransactionAnalysis.create!(txid: btx.txid, cc_code: btx.cc_code, source: 'p2p') unless btx.transaction_analyses.present?
           transaction_source.update! last_processed_blockchain_tx_id: btx.id if btx.id > transaction_source.last_processed_blockchain_tx_id
-        end
+        end.count
+        Rails.logger.info("[LegacyPender] #{count} processed")
         break unless @running
       end
     end
