aa4f160ff73c11d4a9759d29bd9d4627cb90a0c1
Danil Pismenny <danil@brandymint.ru>
2022-02-25 17:26:29 +0300

    m

--- app/models/analyzed_user.rb
+++ app/models/analyzed_user.rb
@@ -2,10 +2,11 @@ class AnalyzedUser < ApplicationRecord
   belongs_to :user
   upsert_keys [:user_id]
 
-  has_many :transaction_analyses
+  # has_many :transaction_analyses
 
   def last_risked_transaction_analysis
-    @last_risked_transaction_analysis ||= transaction_analyses.where(risk_level: 3, risk_confidence: 1.0).last
+    return nil
+    # @last_risked_transaction_analysis ||= transaction_analyses.where(risk_level: 3, risk_confidence: 1.0).last
   end
 
   def to_s
