61a18ea28a12cd2240660c82c5e746a6775b9424
Danil Pismenny <danil@brandymint.ru>
2022-02-28 16:56:05 +0300

    m

--- app/decorators/transaction_source_decorator.rb
+++ app/decorators/transaction_source_decorator.rb
@@ -1,3 +1,11 @@
 class TransactionSourceDecorator < ApplicationDecorator
   delegate_all
+
+  def self.table_columns
+    object_class.attribute_names.map(&:to_sym) + %i[real_blockchain_tx_id]
+  end
+
+  def real_blockchain_tx_id
+    BlockchainTx.where(cc_code: cc_code).maximum(:id)
+  end
 end
