86cbd137ed57814cf21ee25349fdf3a7b71fcd40
Eugene Stepanov <antynitr@gmail.com>
2022-03-01 18:19:51 +0300

    fix

--- db/structure.sql
+++ db/structure.sql
@@ -17,17 +17,17 @@ CREATE SCHEMA meduza;
 
 
 --
--- Name: public; Type: SCHEMA; Schema: -; Owner: -
+-- Name: citext; Type: EXTENSION; Schema: -; Owner: -
 --
 
-CREATE SCHEMA public;
+CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public;
 
 
 --
--- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: -
+-- Name: EXTENSION citext; Type: COMMENT; Schema: -; Owner: -
 --
 
-COMMENT ON SCHEMA public IS 'standard public schema';
+COMMENT ON EXTENSION citext IS 'data type for case-insensitive character strings';
 
 
 SET default_tablespace = '';
@@ -35,12 +35,12 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- Name: address_analyses; Type: TABLE; Schema: meduza; Owner: -
+-- Name: address_analyses; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE meduza.address_analyses (
+CREATE TABLE public.address_analyses (
     id bigint NOT NULL,
-    address meduza.citext NOT NULL,
+    address public.citext NOT NULL,
     risk_level integer NOT NULL,
     risk_confidence numeric NOT NULL,
     analysis_result_id bigint NOT NULL,
@@ -50,10 +50,10 @@ CREATE TABLE meduza.address_analyses (
 
 
 --
--- Name: address_analyses_id_seq; Type: SEQUENCE; Schema: meduza; Owner: -
+-- Name: address_analyses_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
-CREATE SEQUENCE meduza.address_analyses_id_seq
+CREATE SEQUENCE public.address_analyses_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -62,19 +62,19 @@ CREATE SEQUENCE meduza.address_analyses_id_seq
 
 
 --
--- Name: address_analyses_id_seq; Type: SEQUENCE OWNED BY; Schema: meduza; Owner: -
+-- Name: address_analyses_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
-ALTER SEQUENCE meduza.address_analyses_id_seq OWNED BY meduza.address_analyses.id;
+ALTER SEQUENCE public.address_analyses_id_seq OWNED BY public.address_analyses.id;
 
 
 --
--- Name: analysis_results; Type: TABLE; Schema: meduza; Owner: -
+-- Name: analysis_results; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE meduza.analysis_results (
+CREATE TABLE public.analysis_results (
     id bigint NOT NULL,
-    address_transaction meduza.citext NOT NULL,
+    address_transaction public.citext NOT NULL,
     risk_confidence numeric NOT NULL,
     risk_level integer NOT NULL,
     raw_response jsonb NOT NULL,
@@ -86,10 +86,10 @@ CREATE TABLE meduza.analysis_results (
 
 
 --
--- Name: analysis_results_id_seq; Type: SEQUENCE; Schema: meduza; Owner: -
+-- Name: analysis_results_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
-CREATE SEQUENCE meduza.analysis_results_id_seq
+CREATE SEQUENCE public.analysis_results_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -98,17 +98,17 @@ CREATE SEQUENCE meduza.analysis_results_id_seq
 
 
 --
--- Name: analysis_results_id_seq; Type: SEQUENCE OWNED BY; Schema: meduza; Owner: -
+-- Name: analysis_results_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
-ALTER SEQUENCE meduza.analysis_results_id_seq OWNED BY meduza.analysis_results.id;
+ALTER SEQUENCE public.analysis_results_id_seq OWNED BY public.analysis_results.id;
 
 
 --
--- Name: analyzed_users; Type: TABLE; Schema: meduza; Owner: -
+-- Name: analyzed_users; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE meduza.analyzed_users (
+CREATE TABLE public.analyzed_users (
     id bigint NOT NULL,
     user_id bigint NOT NULL,
     risk_level_1_count integer DEFAULT 0 NOT NULL,
@@ -120,10 +120,10 @@ CREATE TABLE meduza.analyzed_users (
 
 
 --
--- Name: analyzed_users_id_seq; Type: SEQUENCE; Schema: meduza; Owner: -
+-- Name: analyzed_users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
-CREATE SEQUENCE meduza.analyzed_users_id_seq
+CREATE SEQUENCE public.analyzed_users_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -132,17 +132,29 @@ CREATE SEQUENCE meduza.analyzed_users_id_seq
 
 
 --
--- Name: analyzed_users_id_seq; Type: SEQUENCE OWNED BY; Schema: meduza; Owner: -
+-- Name: analyzed_users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
-ALTER SEQUENCE meduza.analyzed_users_id_seq OWNED BY meduza.analyzed_users.id;
+ALTER SEQUENCE public.analyzed_users_id_seq OWNED BY public.analyzed_users.id;
 
 
 --
--- Name: pending_analyses; Type: TABLE; Schema: meduza; Owner: -
+-- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE meduza.pending_analyses (
+CREATE TABLE public.ar_internal_metadata (
+    key character varying NOT NULL,
+    value character varying,
+    created_at timestamp(6) without time zone NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL
+);
+
+
+--
+-- Name: pending_analyses; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.pending_analyses (
     id bigint NOT NULL,
     address_transaction character varying NOT NULL,
     state character varying DEFAULT 'pending'::character varying NOT NULL,
@@ -159,10 +171,10 @@ CREATE TABLE meduza.pending_analyses (
 
 
 --
--- Name: pending_analyses_id_seq; Type: SEQUENCE; Schema: meduza; Owner: -
+-- Name: pending_analyses_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
-CREATE SEQUENCE meduza.pending_analyses_id_seq
+CREATE SEQUENCE public.pending_analyses_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -171,19 +183,28 @@ CREATE SEQUENCE meduza.pending_analyses_id_seq
 
 
 --
--- Name: pending_analyses_id_seq; Type: SEQUENCE OWNED BY; Schema: meduza; Owner: -
+-- Name: pending_analyses_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
-ALTER SEQUENCE meduza.pending_analyses_id_seq OWNED BY meduza.pending_analyses.id;
+ALTER SEQUENCE public.pending_analyses_id_seq OWNED BY public.pending_analyses.id;
 
 
 --
--- Name: transaction_analyses; Type: TABLE; Schema: meduza; Owner: -
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE meduza.transaction_analyses (
+CREATE TABLE public.schema_migrations (
+    version character varying NOT NULL
+);
+
+
+--
+-- Name: transaction_analyses; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.transaction_analyses (
     id bigint NOT NULL,
-    txid meduza.citext NOT NULL,
+    txid public.citext NOT NULL,
     cc_code character varying NOT NULL,
     risk_level integer,
     created_at timestamp(6) without time zone NOT NULL,
@@ -197,10 +218,10 @@ CREATE TABLE meduza.transaction_analyses (
 
 
 --
--- Name: transaction_analyses_id_seq; Type: SEQUENCE; Schema: meduza; Owner: -
+-- Name: transaction_analyses_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
-CREATE SEQUENCE meduza.transaction_analyses_id_seq
+CREATE SEQUENCE public.transaction_analyses_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -209,17 +230,17 @@ CREATE SEQUENCE meduza.transaction_analyses_id_seq
 
 
 --
--- Name: transaction_analyses_id_seq; Type: SEQUENCE OWNED BY; Schema: meduza; Owner: -
+-- Name: transaction_analyses_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
-ALTER SEQUENCE meduza.transaction_analyses_id_seq OWNED BY meduza.transaction_analyses.id;
+ALTER SEQUENCE public.transaction_analyses_id_seq OWNED BY public.transaction_analyses.id;
 
 
 --
--- Name: transaction_sources; Type: TABLE; Schema: meduza; Owner: -
+-- Name: transaction_sources; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE meduza.transaction_sources (
+CREATE TABLE public.transaction_sources (
     id bigint NOT NULL,
     last_processed_blockchain_tx_id bigint,
     created_at timestamp(6) without time zone NOT NULL,
@@ -229,10 +250,10 @@ CREATE TABLE meduza.transaction_sources (
 
 
 --
--- Name: transaction_sources_id_seq; Type: SEQUENCE; Schema: meduza; Owner: -
+-- Name: transaction_sources_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
-CREATE SEQUENCE meduza.transaction_sources_id_seq
+CREATE SEQUENCE public.transaction_sources_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -241,121 +262,76 @@ CREATE SEQUENCE meduza.transaction_sources_id_seq
 
 
 --
--- Name: transaction_sources_id_seq; Type: SEQUENCE OWNED BY; Schema: meduza; Owner: -
+-- Name: transaction_sources_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
-ALTER SEQUENCE meduza.transaction_sources_id_seq OWNED BY meduza.transaction_sources.id;
-
-
---
--- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.ar_internal_metadata (
-    key character varying NOT NULL,
-    value character varying,
-    created_at timestamp(6) without time zone NOT NULL,
-    updated_at timestamp(6) without time zone NOT NULL
-);
+ALTER SEQUENCE public.transaction_sources_id_seq OWNED BY public.transaction_sources.id;
 
 
 --
--- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
+-- Name: address_analyses id; Type: DEFAULT; Schema: public; Owner: -
 --
 
-CREATE TABLE public.schema_migrations (
-    version character varying NOT NULL
-);
+ALTER TABLE ONLY public.address_analyses ALTER COLUMN id SET DEFAULT nextval('public.address_analyses_id_seq'::regclass);
 
 
 --
--- Name: address_analyses id; Type: DEFAULT; Schema: meduza; Owner: -
+-- Name: analysis_results id; Type: DEFAULT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY meduza.address_analyses ALTER COLUMN id SET DEFAULT nextval('meduza.address_analyses_id_seq'::regclass);
-
-
---
--- Name: analysis_results id; Type: DEFAULT; Schema: meduza; Owner: -
---
-
-ALTER TABLE ONLY meduza.analysis_results ALTER COLUMN id SET DEFAULT nextval('meduza.analysis_results_id_seq'::regclass);
+ALTER TABLE ONLY public.analysis_results ALTER COLUMN id SET DEFAULT nextval('public.analysis_results_id_seq'::regclass);
 
 
 --
--- Name: analyzed_users id; Type: DEFAULT; Schema: meduza; Owner: -
+-- Name: analyzed_users id; Type: DEFAULT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY meduza.analyzed_users ALTER COLUMN id SET DEFAULT nextval('meduza.analyzed_users_id_seq'::regclass);
-
-
---
--- Name: pending_analyses id; Type: DEFAULT; Schema: meduza; Owner: -
---
-
-ALTER TABLE ONLY meduza.pending_analyses ALTER COLUMN id SET DEFAULT nextval('meduza.pending_analyses_id_seq'::regclass);
+ALTER TABLE ONLY public.analyzed_users ALTER COLUMN id SET DEFAULT nextval('public.analyzed_users_id_seq'::regclass);
 
 
 --
--- Name: transaction_analyses id; Type: DEFAULT; Schema: meduza; Owner: -
+-- Name: pending_analyses id; Type: DEFAULT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY meduza.transaction_analyses ALTER COLUMN id SET DEFAULT nextval('meduza.transaction_analyses_id_seq'::regclass);
-
-
---
--- Name: transaction_sources id; Type: DEFAULT; Schema: meduza; Owner: -
---
-
-ALTER TABLE ONLY meduza.transaction_sources ALTER COLUMN id SET DEFAULT nextval('meduza.transaction_sources_id_seq'::regclass);
+ALTER TABLE ONLY public.pending_analyses ALTER COLUMN id SET DEFAULT nextval('public.pending_analyses_id_seq'::regclass);
 
 
 --
--- Name: address_analyses address_analyses_pkey; Type: CONSTRAINT; Schema: meduza; Owner: -
+-- Name: transaction_analyses id; Type: DEFAULT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY meduza.address_analyses
+ALTER TABLE ONLY public.transaction_analyses ALTER COLUMN id SET DEFAULT nextval('public.transaction_analyses_id_seq'::regclass);
+
+
+--
+-- Name: transaction_sources id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.transaction_sources ALTER COLUMN id SET DEFAULT nextval('public.transaction_sources_id_seq'::regclass);
+
+
+--
+-- Name: address_analyses address_analyses_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.address_analyses
     ADD CONSTRAINT address_analyses_pkey PRIMARY KEY (id);
 
 
 --
--- Name: analysis_results analysis_results_pkey; Type: CONSTRAINT; Schema: meduza; Owner: -
+-- Name: analysis_results analysis_results_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY meduza.analysis_results
+ALTER TABLE ONLY public.analysis_results
     ADD CONSTRAINT analysis_results_pkey PRIMARY KEY (id);
 
 
 --
--- Name: analyzed_users analyzed_users_pkey; Type: CONSTRAINT; Schema: meduza; Owner: -
+-- Name: analyzed_users analyzed_users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY meduza.analyzed_users
+ALTER TABLE ONLY public.analyzed_users
     ADD CONSTRAINT analyzed_users_pkey PRIMARY KEY (id);
-
-
---
--- Name: pending_analyses pending_analyses_pkey; Type: CONSTRAINT; Schema: meduza; Owner: -
---
-
-ALTER TABLE ONLY meduza.pending_analyses
-    ADD CONSTRAINT pending_analyses_pkey PRIMARY KEY (id);
-
-
---
--- Name: transaction_analyses transaction_analyses_pkey; Type: CONSTRAINT; Schema: meduza; Owner: -
---
-
-ALTER TABLE ONLY meduza.transaction_analyses
-    ADD CONSTRAINT transaction_analyses_pkey PRIMARY KEY (id);
-
-
---
--- Name: transaction_sources transaction_sources_pkey; Type: CONSTRAINT; Schema: meduza; Owner: -
---
-
-ALTER TABLE ONLY meduza.transaction_sources
-    ADD CONSTRAINT transaction_sources_pkey PRIMARY KEY (id);
 
 
 --
@@ -367,6 +343,14 @@ ALTER TABLE ONLY public.ar_internal_metadata
 
 
 --
+-- Name: pending_analyses pending_analyses_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.pending_analyses
+    ADD CONSTRAINT pending_analyses_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -375,103 +359,119 @@ ALTER TABLE ONLY public.schema_migrations
 
 
 --
--- Name: index_address_analyses_on_address; Type: INDEX; Schema: meduza; Owner: -
+-- Name: transaction_analyses transaction_analyses_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
-CREATE UNIQUE INDEX index_address_analyses_on_address ON meduza.address_analyses USING btree (address);
-
-
---
--- Name: index_address_analyses_on_analysis_result_id; Type: INDEX; Schema: meduza; Owner: -
---
-
-CREATE INDEX index_address_analyses_on_analysis_result_id ON meduza.address_analyses USING btree (analysis_result_id);
+ALTER TABLE ONLY public.transaction_analyses
+    ADD CONSTRAINT transaction_analyses_pkey PRIMARY KEY (id);
 
 
 --
--- Name: index_analysis_results_on_address_transaction; Type: INDEX; Schema: meduza; Owner: -
+-- Name: transaction_sources transaction_sources_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
-CREATE INDEX index_analysis_results_on_address_transaction ON meduza.analysis_results USING btree (address_transaction);
-
-
---
--- Name: index_analyzed_users_on_risk_level_1_count; Type: INDEX; Schema: meduza; Owner: -
---
-
-CREATE INDEX index_analyzed_users_on_risk_level_1_count ON meduza.analyzed_users USING btree (risk_level_1_count);
+ALTER TABLE ONLY public.transaction_sources
+    ADD CONSTRAINT transaction_sources_pkey PRIMARY KEY (id);
 
 
 --
--- Name: index_analyzed_users_on_risk_level_2_count; Type: INDEX; Schema: meduza; Owner: -
+-- Name: index_address_analyses_on_address; Type: INDEX; Schema: public; Owner: -
 --
 
-CREATE INDEX index_analyzed_users_on_risk_level_2_count ON meduza.analyzed_users USING btree (risk_level_2_count);
-
-
---
--- Name: index_analyzed_users_on_risk_level_3_count; Type: INDEX; Schema: meduza; Owner: -
---
-
-CREATE INDEX index_analyzed_users_on_risk_level_3_count ON meduza.analyzed_users USING btree (risk_level_3_count);
+CREATE UNIQUE INDEX index_address_analyses_on_address ON public.address_analyses USING btree (address);
 
 
 --
--- Name: index_analyzed_users_on_user_id_uniq; Type: INDEX; Schema: meduza; Owner: -
+-- Name: index_address_analyses_on_analysis_result_id; Type: INDEX; Schema: public; Owner: -
 --
 
-CREATE UNIQUE INDEX index_analyzed_users_on_user_id_uniq ON meduza.analyzed_users USING btree (user_id);
-
-
---
--- Name: index_pending_analyses_on_analysis_result_id; Type: INDEX; Schema: meduza; Owner: -
---
-
-CREATE INDEX index_pending_analyses_on_analysis_result_id ON meduza.pending_analyses USING btree (analysis_result_id);
+CREATE INDEX index_address_analyses_on_analysis_result_id ON public.address_analyses USING btree (analysis_result_id);
 
 
 --
--- Name: index_transaction_analyses_on_analysis_result_id; Type: INDEX; Schema: meduza; Owner: -
+-- Name: index_analysis_results_on_address_transaction; Type: INDEX; Schema: public; Owner: -
 --
 
-CREATE INDEX index_transaction_analyses_on_analysis_result_id ON meduza.transaction_analyses USING btree (analysis_result_id);
-
-
---
--- Name: index_transaction_analyses_on_pending_analyses_id; Type: INDEX; Schema: meduza; Owner: -
---
-
-CREATE INDEX index_transaction_analyses_on_pending_analyses_id ON meduza.transaction_analyses USING btree (pending_analyses_id);
+CREATE INDEX index_analysis_results_on_address_transaction ON public.analysis_results USING btree (address_transaction);
 
 
 --
--- Name: index_transaction_analyses_on_txid; Type: INDEX; Schema: meduza; Owner: -
+-- Name: index_analyzed_users_on_risk_level_1_count; Type: INDEX; Schema: public; Owner: -
 --
 
-CREATE UNIQUE INDEX index_transaction_analyses_on_txid ON meduza.transaction_analyses USING btree (txid);
-
-
---
--- Name: pending_analyses fk_rails_27fc4b529c; Type: FK CONSTRAINT; Schema: meduza; Owner: -
---
-
-ALTER TABLE ONLY meduza.pending_analyses
-    ADD CONSTRAINT fk_rails_27fc4b529c FOREIGN KEY (analysis_result_id) REFERENCES meduza.analysis_results(id);
+CREATE INDEX index_analyzed_users_on_risk_level_1_count ON public.analyzed_users USING btree (risk_level_1_count);
 
 
 --
--- Name: address_analyses fk_rails_4d26b9d298; Type: FK CONSTRAINT; Schema: meduza; Owner: -
+-- Name: index_analyzed_users_on_risk_level_2_count; Type: INDEX; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY meduza.address_analyses
-    ADD CONSTRAINT fk_rails_4d26b9d298 FOREIGN KEY (analysis_result_id) REFERENCES meduza.analysis_results(id);
+CREATE INDEX index_analyzed_users_on_risk_level_2_count ON public.analyzed_users USING btree (risk_level_2_count);
+
+
+--
+-- Name: index_analyzed_users_on_risk_level_3_count; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_analyzed_users_on_risk_level_3_count ON public.analyzed_users USING btree (risk_level_3_count);
+
+
+--
+-- Name: index_analyzed_users_on_user_id_uniq; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX index_analyzed_users_on_user_id_uniq ON public.analyzed_users USING btree (user_id);
+
+
+--
+-- Name: index_pending_analyses_on_analysis_result_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_pending_analyses_on_analysis_result_id ON public.pending_analyses USING btree (analysis_result_id);
+
+
+--
+-- Name: index_transaction_analyses_on_analysis_result_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_transaction_analyses_on_analysis_result_id ON public.transaction_analyses USING btree (analysis_result_id);
+
+
+--
+-- Name: index_transaction_analyses_on_pending_analyses_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_transaction_analyses_on_pending_analyses_id ON public.transaction_analyses USING btree (pending_analyses_id);
+
+
+--
+-- Name: index_transaction_analyses_on_txid; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX index_transaction_analyses_on_txid ON public.transaction_analyses USING btree (txid);
+
+
+--
+-- Name: pending_analyses fk_rails_27fc4b529c; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.pending_analyses
+    ADD CONSTRAINT fk_rails_27fc4b529c FOREIGN KEY (analysis_result_id) REFERENCES public.analysis_results(id);
+
+
+--
+-- Name: address_analyses fk_rails_4d26b9d298; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.address_analyses
+    ADD CONSTRAINT fk_rails_4d26b9d298 FOREIGN KEY (analysis_result_id) REFERENCES public.analysis_results(id);
 
 
 --
 -- PostgreSQL database dump complete
 --
 
-SET search_path TO meduza,public;
+SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
 ('20211118152519'),
