29fc1b0ca8f5e90b1a3e8b7fecf27d54a2abd21a
Danil Pismenny <danil@brandymint.ru>
2022-03-02 16:47:12 +0300

    fix

--- db/migrate/20220302134255_add_is_replyied_to_pending_analyses.rb
+++ db/migrate/20220302134255_add_is_replyied_to_pending_analyses.rb
@@ -1,6 +1,6 @@
 class AddIsReplyiedToPendingAnalyses < ActiveRecord::Migration[6.1]
   def change
-    add_column :pending_analyses, :replied_at, :timestamp, null: false
+    add_column :pending_analyses, :replied_at, :timestamp
     PendingAnalysis.where(state: %i[done skippet]).update_all 'replied_at = updated_at'
   end
 end
