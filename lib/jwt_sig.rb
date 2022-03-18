4d022a140bd3edd18a90691edeea11f1c3447e62
Eugene Stepanov <antynitr@gmail.com>
2022-03-18 16:43:03 +0300

    Use MEDUZA_PRIVATE_KEY for sign request

--- lib/jwt_sig.rb
+++ lib/jwt_sig.rb
@@ -1,14 +1,9 @@
 class JWTSig
   ALGO = 'ES256'
-  EXPIRE = 10
 
   class << self
-    def feeze_sig(jwt: JSON.parse(ENV.fetch('P2P_API_FREEZE_JWK')), claims: {})
-      new(jwt: jwt, claims: {})
-    end
-
-    def adm_sig(jwt: JSON.parse(ENV.fetch('P2P_API_ADM_JWK')), claims: {})
-      new(jwt: jwt, claims: {iat: Time.now.to_i, jti: SecureRandom.hex(10), aud: 'adm'})
+    def meduza_sig(jwt: JSON.parse(ENV.fetch('MEDUZA_PRIVATE_KEY')), claims: {})
+      new(jwt: jwt, claims: { iss: 'meduza'})
     end
   end
 
