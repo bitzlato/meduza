1f939fa17439d0558b53e6b1e9cd67986111a66d
Danil Pismenny <danil@brandymint.ru>
2022-03-18 20:31:35 +0300

    Remove legacy code

--- app/daemons/daemons/legacy_pender.rb
+++ app/daemons/daemons/legacy_pender.rb
@@ -4,18 +4,14 @@ module Daemons
   # Берёт все не обработанные транзакции из P2P blockchain_tx и засовывает в pending_transactions
   class LegacyPender < Base
     @sleep_time = 1.seconds
-    LIMIT = 20
+    LIMIT = 100
 
     CHECK_START_DATE = Date.parse('01-10-2021')
-
-    attr_reader :reply_queue
 
     # TODO Проверять в одной валеговской транзкции сразу все транзакции по разным валютам
     def process
       AML_ANALYZABLE_CODES.each do |cc_code|
-        transaction_source = TransactionSource.find_or_create_by!(cc_code: cc_code)
-        transaction_source.reload
-        Rails.logger.debug("[LegacyPender] Select #{cc_code} from #{transaction_source.last_processed_blockchain_tx_id}")
+        Rails.logger.debug("[LegacyPender] Select #{cc_code}")
         btx_count = BlockchainTx
           .receive
           .where('created_at>=?', CHECK_START_DATE)
@@ -24,34 +20,21 @@ module Daemons
           .order(:id)
           .limit(LIMIT)
           .each do |btx|
-            Rails.logger.info("[LegacyPender] Put pending analysis #{btx.id}: #{btx.txid} #{cc_code}")
-            payload = {
-              txid:    btx.txid,
-              cc_code: btx.cc_code,
-              source:  'p2p',
-              meta: { blockchain_tx_id: btx.id }
-            }
-            btx.update! meduza_status: { status: :pended }
-            AMQP::Queue.publish :meduza, payload,
-              correlation_id: btx.id,
-              routing_key: AMQP::Config.binding(:transaction_pender).fetch(:routing_key),
-              reply_to: AMQP::Config.binding(:legacy_rpc_callback).fetch(:routing_key)
-            transaction_source.update! last_processed_blockchain_tx_id: btx.id if btx.id > transaction_source.last_processed_blockchain_tx_id
-          end.count
+          Rails.logger.info("[LegacyPender] Put pending analysis #{btx.id}: #{btx.txid} #{cc_code}")
+          payload = {
+            txid:    btx.txid,
+            cc_code: btx.cc_code,
+            source:  'p2p',
+            meta: { blockchain_tx_id: btx.id }
+          }
+          btx.update! meduza_status: { status: :pended }
+          AMQP::Queue.publish :meduza, payload,
+            correlation_id: btx.id,
+            routing_key: AMQP::Config.binding(:transaction_pender).fetch(:routing_key),
+            reply_to: AMQP::Config.binding(:legacy_rpc_callback).fetch(:routing_key)
+        end.count
         Rails.logger.debug("[LegacyPender] #{btx_count} processed for #{cc_code}")
         break unless @running
-      end
-    end
-
-    private
-
-    def setup_reply_queue
-      bunny_conn = Bunny.new AMQP::Config.connect
-      bunny_conn.start
-      channel = bunny_conn.create_channel
-      @reply_queue = channel.queue('', exclusive: true)
-      @reply_queue.subscribe do |_delivery_info, properties, payload|
-        Rails.logger.info "[LegacyPender] Receive reply_to with payload #{payload} and properties #{properties}"
       end
     end
   end
