c0377a6f7493c7f3301f36d27abc089e97269f85
Danil Pismenny <danil@brandymint.ru>
2022-03-31 18:42:12 +0300

    Find or create TransactionPender

--- app/workers/amqp/transaction_pender.rb
+++ app/workers/amqp/transaction_pender.rb
@@ -4,23 +4,6 @@ module AMQP
   class TransactionPender < Base
     def process(payload, metadata)
       Rails.logger.info "[TransactionChecker] payload=#{payload}, metadata=#{metadata}"
-      if PendingAnalysis.pending.find_by(
-        address_transaction: payload.fetch('txid'),
-        cc_code:             payload.fetch('cc_code'),
-        source:              payload.fetch('source'),
-      ).present?
-        # TODO отвечать сразу если есть
-        Rails.logger.info("[TransactionChecker] Skip #{payload}")
-        payload = {
-          address_transaction: payload.fetch('txid'),
-          cc_code: payload.fetch('cc_code'),
-          action: :queued
-        }
-        properties = { routing_key: metadata.reply_to, correlation_id:  metadata.correlation_id }
-        Rails.logger.info "[TransactionAnalysis] rpc_callback with payload #{payload} and properties #{properties}"
-        AMQP::Queue.publish :meduza, payload, properties
-        return
-      end
       ta = TransactionAnalysis.find_by(txid: payload.fetch('txid'), cc_code: payload.fetch('cc_code'))
       if ta.present?
         payload = {
@@ -34,16 +17,23 @@ module AMQP
         Rails.logger.info "[TransactionAnalysis] rpc_callback with payload #{payload} and properties #{properties}"
         AMQP::Queue.publish :meduza, payload, properties
       else
-        PendingAnalysis.
-          create!(
+        attrs = {
+          meta:                payload.fetch('meta', {}),
+          type:                payload.fetch('type', :transaction),
+          reply_to:            metadata.reply_to,
+          correlation_id:      metadata.correlation_id,
+        }
+        pa = PendingAnalysis.
+          create_with(attrs).
+          find_or_create_by!(
             address_transaction: payload.fetch('txid'),
             cc_code:             payload.fetch('cc_code'),
             source:              payload.fetch('source'),
-            meta:                payload.fetch('meta', {}),
-            type:                payload.fetch('type', :transaction),
-            reply_to:            metadata.reply_to,
-            correlation_id:      metadata.correlation_id,
+            status:              :pending,
         )
+        finded_attrs = pa.attributes.slice(*attrs.keys.map(&:to_s))
+        diff = HashDiff::Comparison.new( attrs, finded_attrs ).diff
+        report_exception 'WTF', true, { attr: attrs, finded_attrs: finded_attrs, diff: diff } if diff.present?
       end
     end
   end
