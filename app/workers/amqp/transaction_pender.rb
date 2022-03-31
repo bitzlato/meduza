679797812cc10bc164d2ec87ab1090125c7dded2
Danil Pismenny <danil@brandymint.ru>
2022-03-31 19:30:09 +0300

    m

--- app/workers/amqp/transaction_pender.rb
+++ app/workers/amqp/transaction_pender.rb
@@ -35,8 +35,8 @@ module AMQP
               state:              :pending,
           )
           finded_attrs = pa.attributes.slice(*attrs.keys.map(&:to_s))
-          diff = HashDiff::Comparison.new( attrs, finded_attrs ).diff
-          report_exception 'WTF', true, { attr: attrs, finded_attrs: finded_attrs, diff: diff } if diff.present?
+          diff = HashDiff::Comparison.new( attrs, finded_attrs.symbolize_keys ).diff
+          report_exception 'WTF', true, { pending_analisis_id: pa.id, attr: attrs, finded_attrs: finded_attrs, diff: diff } if diff.present?
         rescue ActiveRecord::RecordInvalid => err
           Rails.logger.info "[TransactionPender] PendingAnalysis already exists #{payload} with error #{err}"
         end
