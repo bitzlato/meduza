4c95678e02995aabd23dfd04ec257b27880cea63
Danil Pismenny <danil@brandymint.ru>
2022-04-07 21:52:54 +0300

    Minor improve withdraws

--- app/daemons/daemons/legacy_pender.rb
+++ app/daemons/daemons/legacy_pender.rb
@@ -8,30 +8,77 @@ module Daemons
     MAX_PENDING_QUEUE_SIZE = 5
     CHECK_START_DATE = Date.parse('01-10-2021')
 
-    # TODO Проверять в одной валеговской транзкции сразу все транзакции по разным валютам
     def process
+      logger.tagged('LegacyPender') do
+        process_transactions
+        # process_withdrawals
+      end
+    end
+
+    def process_withdrawals
       AML_ANALYZABLE_CODES.each do |cc_code|
-        scope = BlockchainTx
+        scope = Withdrawal
           .where('created_at>=?', CHECK_START_DATE)
           .where(meduza_status: nil)
           .where(cc_code: cc_code)
-        Rails.logger.info("[LegacyPender] Select #{cc_code} count is #{scope.count}")
+        logger.info("Select #{cc_code} count is #{scope.count}")
         btx_count = scope
           .order(:id)
           .limit(LIMIT)
           .each do |btx|
           next if PendingAnalysis.pending.where(cc_code: cc_code).count > MAX_PENDING_QUEUE_SIZE
           if PendingAnalysis.pending.exists?(address_transaction: btx.txid, cc_code: btx.cc_code)
-            Rails.logger.info("[LegacyPender] PendingAnalysis already exists #{btx.txid}")
+            logger.info("PendingAnalysis already exists #{btx.txid}")
             return
           end
 
           ta = TransactionAnalysis.find_by(txid: btx.txid, cc_code: btx.cc_code)
           if ta.present?
-            Rails.logger.info("[LegacyPender] TransactionAnalysis already exists #{btx.txid} update blockhain_tx")
+            logger.info("TransactionAnalysis already exists #{btx.txid} update blockhain_tx")
             ta.update_blockchain_tx_status
           else
-            Rails.logger.info("[LegacyPender] Put pending analysis #{btx.id}: #{btx.txid} #{cc_code}")
+            logger.info("Put pending analysis #{btx.id}: #{btx.txid} #{cc_code}")
+            payload = {
+              txid:    btx.txid,
+              cc_code: btx.cc_code,
+              source:  'p2p',
+              meta: { blockchain_tx_id: btx.id, sent_at: Time.zone.now }
+            }
+            AMQP::Queue.publish :meduza, payload,
+              correlation_id: btx.id,
+              routing_key: AMQP::Config.binding(:transaction_pender).fetch(:routing_key),
+              reply_to: AMQP::Config.binding(:legacy_rpc_callback).fetch(:routing_key)
+            btx.update! meduza_status: { status: :pended }
+          end
+        end.count
+        logger.debug("#{btx_count} processed for #{cc_code}")
+        break unless @running
+      end
+    end
+
+    def process_transactions
+      AML_ANALYZABLE_CODES.each do |cc_code|
+        scope = BlockchainTx
+          .where('created_at>=?', CHECK_START_DATE)
+          .where(meduza_status: nil)
+          .where(cc_code: cc_code)
+        logger.info("Select #{cc_code} count is #{scope.count}")
+        btx_count = scope
+          .order(:id)
+          .limit(LIMIT)
+          .each do |btx|
+          next if PendingAnalysis.pending.where(cc_code: cc_code).count > MAX_PENDING_QUEUE_SIZE
+          if PendingAnalysis.pending.exists?(address_transaction: btx.txid, cc_code: btx.cc_code)
+            logger.info("PendingAnalysis already exists #{btx.txid}")
+            return
+          end
+
+          ta = TransactionAnalysis.find_by(txid: btx.txid, cc_code: btx.cc_code)
+          if ta.present?
+            logger.info("TransactionAnalysis already exists #{btx.txid} update blockhain_tx")
+            ta.update_blockchain_tx_status
+          else
+            logger.info("Put pending analysis #{btx.id}: #{btx.txid} #{cc_code}")
             payload = {
               txid:    btx.txid,
               cc_code: btx.cc_code,
