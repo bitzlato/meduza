2101b6a80fd228f144ec8cca8f6a34ece40413ab
Danil Pismenny <danil@brandymint.ru>
2022-04-08 13:00:33 +0300

    Fix bitzlato_api

--- lib/bitzlato_api.rb
+++ lib/bitzlato_api.rb
@@ -3,13 +3,13 @@ class BitzlatoAPI
   attr_reader :url, :debug
 
   def initialize(url: ENV.fetch('BITZLATO_API_URL'), debug: ENV.true?('BITZLATO_CURL_LOGGER'))
-    @url = url
+    @url = URI url
     @debug = debug
   end
 
   def freeze_user(user_id, params: {}, connection: nil, claims: {})
     connection ||= build_connection(claims)
-    connection.post("/admin/p2p/freeze/#{user_id}/", JSON.generate(params))
+    connection.post(url.path + "/admin/p2p/freeze/#{user_id}/", JSON.generate(params))
   end
 
   private
