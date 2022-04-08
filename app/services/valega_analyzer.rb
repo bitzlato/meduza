61e8f0063a2f9765c00295ed7b0a978d734b71f6
Danil Pismenny <danil@brandymint.ru>
2022-04-08 16:36:46 +0300

    m

--- app/services/valega_analyzer.rb
+++ app/services/valega_analyzer.rb
@@ -18,7 +18,7 @@ class ValegaAnalyzer
       .risk_analysis(address_transactions: address_transactions, asset_type_id: ValegaClient.get_asset_type_id(cc_code))
       .map do |response|
         # {"result":true,"data":[{"value":"00","error":"Please analyse BTC address. Please try again!"}]}
-        raise CheckError.new, response.fetch('error'), response if response.key? 'error'
+        raise CheckError.new(response.fetch('error'), response) if response.key? 'error'
         risks = response.slice('risk_level', 'risk_confidence')
 
         AnalysisResult.create! risks.merge(address_transaction: response.fetch('value'), cc_code: cc_code, raw_response: response, type: response.fetch('type'))
