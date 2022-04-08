38b8f216a7bd9f2fb6f33f8c6c547a5669f96232
Danil Pismenny <danil@brandymint.ru>
2022-04-08 22:16:17 +0300

    m

--- app/controllers/address_analyses_controller.rb
+++ app/controllers/address_analyses_controller.rb
@@ -1,6 +1,6 @@
 class AddressAnalysesController < ResourcesController
   def recheck
     record.recheck!
-    redirect_to address_analyses_path(record), notice: t('.recheck_pended')
+    redirect_to address_analysis_path(record), notice: t('.recheck_pended')
   end
 end
