b34e0ecb964ba0328ddc05852153b6aeab042575
Danil Pismenny <danil@brandymint.ru>
2022-04-08 13:16:22 +0300

    Improve bitzlato_api logging

--- lib/bitzlato_api.rb
+++ lib/bitzlato_api.rb
@@ -2,11 +2,13 @@ require 'jwt_sig'
 require 'faraday/detailed_logger'
 
 class BitzlatoAPI
-  attr_reader :url, :debug
+  attr_reader :url, :debug, :logger
 
   def initialize(url: ENV.fetch('BITZLATO_API_URL'), debug: ENV.true?('BITZLATO_CURL_LOGGER'))
     @url = URI url
     @debug = debug
+    @logger = Rails.application.root.join('log/bitzlato_api.log')
+    @logger.level = Logger::DEBUG
   end
 
   def freeze_user(user_id, params: {}, connection: nil, claims: {})
@@ -23,10 +25,7 @@ class BitzlatoAPI
         'Content-Type' => 'application/json',
         'Accept' => 'application/json',
       }
-      if debug
-        c.response :detailed_logger, Rails.logger
-        # c.request :curl, Rails.logger, :debug
-      end
+      c.response :detailed_logger, logger
       c.request :authorization, 'Bearer', JWTSig.meduza_sig.encode(claims)
       yield c if block_given?
     end
