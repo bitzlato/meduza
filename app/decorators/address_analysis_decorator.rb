858f257274c6049b3815ff54e71d8f966fa9d311
Danil Pismenny <danil@brandymint.ru>
2022-04-08 17:36:28 +0300

    m

--- app/decorators/address_analysis_decorator.rb
+++ app/decorators/address_analysis_decorator.rb
@@ -2,6 +2,6 @@ class AddressAnalysisDecorator < ApplicationDecorator
   delegate_all
 
   def self.table_columns
-    %i[id address cc_code risk_level risk_confidence analysis_result_message created_at updated_at]
+    %i[id address cc_code risk_level risk_confidence entity_name entity_dir_name analysis_result_message created_at updated_at]
   end
 end
--- app/decorators/transaction_analysis_decorator.rb
+++ app/decorators/transaction_analysis_decorator.rb
@@ -2,7 +2,7 @@ class TransactionAnalysisDecorator < ApplicationDecorator
   delegate_all
 
   def self.table_columns
-    %i[updated_at direction cc_code txid amount blockchain_tx risk_level risk_confidence analysis_result_message]
+    %i[updated_at direction cc_code txid amount blockchain_tx risk_level risk_confidence entity_name entity_dir_name analysis_result_message]
   end
 
   def blockchain_tx
