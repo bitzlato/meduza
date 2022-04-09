87aef564ec588430189599186f2d57efdc75514a
Danil Pismenny <danil@brandymint.ru>
2022-04-09 23:09:26 +0300

    Expoxe link to danger addresses in analyzed user decorator

--- app/decorators/analyzed_user_decorator.rb
+++ app/decorators/analyzed_user_decorator.rb
@@ -5,16 +5,14 @@ class AnalyzedUserDecorator < ApplicationDecorator
     %i[user_id updated_at danger_transactions_count danger_addresses_count]
   end
 
+  def danger_addresses_count
+    h.link_to object.danger_addresses_count,
+      h.danger_addresses_path(q: { analyzed_user_id_eq: object.id } )
+  end
+
   def danger_transactions_count
     h.link_to object.danger_transactions_count,
       h.danger_transactions_path(q: { analyzed_user_id_eq: object.id } )
-  end
-
-  def danger_transactions_count_fake
-    h.link_to object.danger_transactions_count,
-      h.transaction_analyses_path(q: { analyzed_user_id_eq: object.id,
-                                       risk_level_eq: ValegaAnalyzer::DANGER_RISK_LEVEL,
-                                       risk_confidence_eq: ValegaAnalyzer::DANGER_RISK_CONFIDENCE })
   end
 
   def last_risked_transaction_analysis
