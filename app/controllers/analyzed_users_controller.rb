24080b842cfefbd2997d97e1c80e1908204c0c45
Danil Pismenny <danil@brandymint.ru>
2022-04-09 23:17:57 +0300

    Fix danger index form sort

--- app/controllers/analyzed_users_controller.rb
+++ app/controllers/analyzed_users_controller.rb
@@ -3,6 +3,6 @@ class AnalyzedUsersController < ResourcesController
   private
 
   def default_sort
-    'danger_transactions_count desc'
+    'danger_transactions_count desc, danger_addresses_count desc'
   end
 end
