984c0bb61f04539ac62ad3c9ad4d800216b226a4
Danil Pismenny <danil@brandymint.ru>
2022-04-10 23:16:23 +0300

    minor

--- lib/valega_client.rb
+++ lib/valega_client.rb
@@ -7,7 +7,7 @@ class ValegaClient
   # 5 секунд ограничение от valega, добавляем еще 1 секунду на всякий случай
   PAUSE_BETWEEN_REQUESTS = 6.seconds
 
-  READ_TIMEOUT = 10 # seconds
+  READ_TIMEOUT = 60 # seconds
 
   Error = Class.new StandardError
   UnknownError = Class.new Error
