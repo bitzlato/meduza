14309e833b37ebbffccc324686c056e3b366a751
Danil Pismenny <danil@brandymint.ru>
2022-04-10 22:57:02 +0300

    m

--- app/daemons/daemons/legacy_pender.rb
+++ app/daemons/daemons/legacy_pender.rb
@@ -101,7 +101,7 @@ module Daemons
                 correlation_id: btx.id,
                 routing_key: AMQP::Config.binding(:transaction_pender).fetch(:routing_key),
                 reply_to: AMQP::Config.binding(:legacy_rpc_callback).fetch(:routing_key)
-              btx.update! meduza_status: { status: :pended, pended_at: Timez.zone.now.iso8601 }
+              btx.update! meduza_status: { status: :pended, pended_at: Time.zone.now.iso8601 }
             end
           end.count
           Rails.logger.debug("[LegacyPender] #{btx_count} processed for #{cc_code}")
