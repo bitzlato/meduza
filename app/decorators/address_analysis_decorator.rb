5da53141b650c11a406096a767b629c6ff5d8e00
Danil Pismenny <danil@brandymint.ru>
2022-04-11 13:51:35 +0300

    Fix address link

--- app/decorators/address_analysis_decorator.rb
+++ app/decorators/address_analysis_decorator.rb
@@ -4,4 +4,12 @@ class AddressAnalysisDecorator < ApplicationDecorator
   def self.table_columns
     %i[id address cc_code risk_level risk_confidence entity_name entity_dir_name analysis_result_message created_at updated_at]
   end
+
+  def address
+    return h.middot if object.address.nil?
+
+    h.link_to h.address_analysis_path(object.id) do
+      h.present_address object.address
+    end
+  end
 end
