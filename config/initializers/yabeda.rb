54793f48c587609c068696effc8966a45e4cc49e
Danil Pismenny <danil@brandymint.ru>
2022-04-15 22:01:52 +0300

    Fix yabeda collect

--- config/initializers/yabeda.rb
+++ config/initializers/yabeda.rb
@@ -4,9 +4,11 @@ Yabeda.configure do
   counter :checked_pending_analyses,
     comment: 'A counter of checked PendingAnalyses',
     tags: %i[cc_code type risk_level]
+
   gauge :pending_analyses_queue_size,
     comment: 'A size on PendingAnalyses queue',
     tags: %i[cc_code type]
+
   histogram :valega_analyzation_runtime,
     comment: 'How long Valega analyze data',
     buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10, 30],
@@ -14,8 +16,14 @@ Yabeda.configure do
     unit: :ms
 
   collect do
-    PendingAnalysis.pending.group(:cc_code, :type).count.each do |( cc_code, type ), size|
-      meduza.pending_analyses_queue_size.set({cc_code: cc_code, type: type}, size)
+    Currency.
+      cc_codes.
+      map { |cc_code| PendingAnalysis::TYPES.map { |type| [cc_code, type] } }.
+      each_with_object({}) { |keys, ag| keys.each { |key| ag[key]= 0 } }.
+      merge(PendingAnalysis.pending.group(:cc_code, :type).count).
+      each do |( cc_code, type ), size|
+      puts [cc_code, type, size].join(';')
+      # meduza.pending_analyses_queue_size.set({cc_code: cc_code, type: type}, size)
     end
   end
 end
