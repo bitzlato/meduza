588facf39a3faacaa5dbce2bb3cd1989ff094abf
Aleksandr B. Ivanov <aleksandr.i@bitzlato.com>
2022-09-13 15:27:21 +0300

    [#182631233] Remove withdrawal checker

--- app/models/withdrawal.rb
+++ app/models/withdrawal.rb
@@ -8,13 +8,4 @@ class Withdrawal < ApplicationRecord
   belongs_to :blockchain_tx
 
   belongs_to :currency, foreign_key: :cc_code, primary_key: :cc_code
-
-  scope :aml, -> { where status: :aml }
-
-  def pending!(meduza_status)
-    with_lock do
-      raise WrongStatus, "with_lock wrong status #{status}, skip [id: #{id}]" unless status == 'aml'
-      update_columns status: :pending, meduza_status: meduza_status
-    end
-  end
 end
