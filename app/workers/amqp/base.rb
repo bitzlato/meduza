610d7d049c7135b6305c49c97b3a200c05a5ea0c
Eugene Stepanov <antynitr@gmail.com>
2022-12-26 17:14:08 +0300

    Lookup blockchain name

--- app/workers/amqp/base.rb
+++ app/workers/amqp/base.rb
@@ -4,8 +4,30 @@ module AMQP
   class Base # rubocop:disable Lint/EmptyClass
     attr_reader :logger
 
+    BLOCKCHAIN_MAP = {
+      'btc' => 'BITCOIN',
+      'polygon-mainnet' => 'POLYGON',
+      'bsc-mainnet' => 'BSC',
+      'heco-mainnet' => 'HECO',
+      'tron-mainnet' => 'TRON',
+      'bitoto-tron-mainnet' => 'TRON',
+      'eth-mainnet' => 'ETHEREUM',
+      'p2p-doge-mainnet' => 'DOGE',
+      'p2p-eth-mainnet' => 'ETHEREUM',
+      'p2p-dash-mainnet' => 'DASH',
+      'p2p-ltc-mainnet' => 'LITECOIN',
+      'p2p-bch-mainnet' => 'BITCOINCASH',
+      'p2p-btc-mainnet' => 'BITCOIN',
+      'p2p-tron-mainnet' => 'TRON',
+      'p2p-bsc-mainnet' => 'BSC'
+    }
+
     def initialize
       @logger = Rails.logger.tagged self.class.name
+    end
+
+    def lookup_blockchain(blockchain)
+      BLOCKCHAIN_MAP[blockchain] || blockchain
     end
   end
 end
