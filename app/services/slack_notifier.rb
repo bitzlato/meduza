c5171b505b37ed5f4f1d08e403f8f8abfe13ab60
Eugene Stepanov <antynitr@gmail.com>
2022-12-30 17:44:55 +0300

    Add slack notifier

---
+++ app/services/slack_notifier.rb
@@ -0,0 +1,32 @@
+class SlackNotifier
+  module FakeNotifier
+    def self.ping(*args)
+      Rails.logger.debug("SlackNotifier received ping with args #{args}")
+    end
+
+    def self.post(*args)
+      Rails.logger.debug("SlackNotifier received post with args #{args}")
+    end
+  end
+
+  attr_reader :notifier
+  delegate :ping, :post, to: :notifier
+
+  def self.notifications
+    new ENV.fetch('SLACK_NOTIFICATIONS_CHANNEL')
+  end
+
+  def initialize(channel)
+    @notifier = build_notifier(channel)
+  end
+
+  private
+
+  def build_notifier(channel)
+    return FakeNotifier if Rails.env.test?
+    return FakeNotifier unless ENV.key? 'SLACK_WEBHOOK_URL'
+    Slack::Notifier.new ENV.fetch('SLACK_WEBHOOK_URL') do
+      defaults channel: channel
+    end
+  end
+end
