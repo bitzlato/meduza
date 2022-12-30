b5aac19aacc9aafd2d38ca6fbe95a66caa261b0f
Eugene Stepanov <antynitr@gmail.com>
2022-12-30 18:03:08 +0300

    fix

--- app/services/slack_notifier.rb
+++ app/services/slack_notifier.rb
@@ -1,15 +1,16 @@
 class SlackNotifier
   module FakeNotifier
     def self.ping(*args)
-      Rails.logger.debug("SlackNotifier received ping with args #{args}")
+      Rails.logger.debug { "SlackNotifier received ping with args #{args}" }
     end
 
     def self.post(*args)
-      Rails.logger.debug("SlackNotifier received post with args #{args}")
+      Rails.logger.debug { "SlackNotifier received post with args #{args}" }
     end
   end
 
   attr_reader :notifier
+
   delegate :ping, :post, to: :notifier
 
   def self.notifications
@@ -25,6 +26,7 @@ class SlackNotifier
   def build_notifier(channel)
     return FakeNotifier if Rails.env.test?
     return FakeNotifier unless ENV.key? 'SLACK_WEBHOOK_URL'
+
     Slack::Notifier.new ENV.fetch('SLACK_WEBHOOK_URL') do
       defaults channel: channel
     end
