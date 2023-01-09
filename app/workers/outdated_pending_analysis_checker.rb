3618afc0d0831beec3191fc3f980b2a8efaa00c9
Eugene. S <antynitr@gmail.com>
2023-01-09 14:32:50 +0300

    Outdated pending analysis notifier v3 (#30)
    * Add slack notifier
    
    * Add Outdated pending analysis checker job
    
    * Check and notifiy about new stacked pending analysis to slack

---
+++ app/workers/outdated_pending_analysis_checker.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class OutdatedPendingAnalysisChecker < ApplicationJob
+  CACHE_KEY = 'outdated_pending_analysis'
+
+  def perform
+    pending_analysis = PendingAnalysis.pending.outdated
+
+    ids = pending_analysis.ids
+    old_ids = retive_ids
+    new_ids = ids - old_ids
+
+    if new_ids.any?
+      message = %(:warning: Есть новые зависшие проверки\n#{new_ids.map { |id| "https://meduza.lgk.one/pending_analyses/#{id}" }.join("\n")})
+      message << "\n Всего зависло #{ids.size}"
+      SlackNotifier.notifications.ping(message)
+    end
+
+    update_ids(ids)
+  end
+
+  private
+
+  # rubocop:disable Security/MarshalLoad
+  def retive_ids
+    redis.get(CACHE_KEY).then { |res| res.present? ? Marshal.load(res) : [] }
+  end
+  # rubocop:enable Security/MarshalLoad
+
+  def update_ids(ids)
+    redis.set(CACHE_KEY, Marshal.dump(ids))
+  end
+
+  def redis
+    @redis ||= Redis.new url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1')
+  end
+end
